d65bbf5370e2fc6829d993b05be1d5adbc5a9dd2
Thiago Trad <thiago_trad@hotmail.com>
2021-11-07 16:24:58 -0300

    FIX : Characters without images

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -20,7 +20,3 @@ const Students = ({ contestants, handleWinner }) => {
 };
 
 export default Students;
-
-// {studentList.map((student, index) => (
-//      />
-//   ))}
--- src/components/StudentsCard/index.jsx
+++ src/components/StudentsCard/index.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "./style.css";
+import { useState } from "react";
 
 const StudentsCard = ({ student }) => {
+  const [imgExists, setimgExists] = useState(true);
+  if (student.image.lenght === 0) {
+    setimgExists(false);
+  }
   return (
     <div>
       <div className={`card ${student.house.replace(/"/g, "")}`}>
-        <img
-          className={`picture ${student.house.replace(/"/g, "")}`}
-          src={student.image}
-          alt={student.name}
-        />
+        {imgExists ? (
+          <img
+            className={`picture ${student.house.replace(/"/g, "")}`}
+            src={student.image}
+            alt={student.name}
+          />
+        ) : (
+          <img
+            className={`picture ${student.house.replace(/"/g, "")}`}
+            src={`../images/${student.house.replace(/"/g, "")}.jpeg`}
+            alt={student.name}
+          />
+        )}
+
         <label>{student.name}</label>
         <p>{student.house}</p>
       </div>
